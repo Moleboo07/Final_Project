01a51d8ec3af2c387f8adaa0ebe9f8c55e268f27
Mendrika-jolivet <94170951+Mendrika-jolivet@users.noreply.github.com>
2023-05-11 11:20:25 +0200

    Update web-app.py

--- streamlit/web-app.py
+++ streamlit/web-app.py
@@ -35,6 +35,6 @@ st.bar_chart(chart_data.set_index('Intitulé du réseau'))
 
 st.markdown('''
     <iframe src="https://opendata.plus.transformation.gouv.fr/explore/embed/dataset/export-experiences/analyze/?dataChart=eyJxdWVyaWVzIjpbeyJjb25maWciOnsiZGF0YXNldCI6ImV4cG9ydC1leHBlcmllbmNlcyIsIm9wdGlvbnMiOnt9fSwiY2hhcnRzIjpbeyJhbGlnbk1vbnRoIjp0cnVlLCJ0eXBlIjoibGluZSIsImZ1bmMiOiJDT1VOVCIsInlBeGlzIjoiaWRfZXhwZXJpZW5jZSIsInNjaWVudGlmaWNEaXNwbGF5Ijp0cnVlLCJjb2xvciI6InJhbmdlLUFjY2VudCJ9XSwieEF4aXMiOiJkYXRlX2FjdGlvbl9lbmdhZ2VlIiwibWF4cG9pbnRzIjoiIiwidGltZXNjYWxlIjoieWVhciIsInNvcnQiOiIiLCJzZXJpZXNCcmVha2Rvd24iOiJldmFsdWF0aW9uX3V0aWxlX3JlcG9uc2Vfc3RydWN0dXJlXzFfcGFyX3Zpc2l0ZXVycyJ9XSwiZGlzcGxheUxlZ2VuZCI6dHJ1ZSwiYWxpZ25Nb250aCI6dHJ1ZX0%3D&static=false&datasetcard=false" 
-    width="400" height="300" frameborder="0"></iframe>
+    width="800" height="400" frameborder="0"></iframe>
 ''', unsafe_allow_html=True)
 
